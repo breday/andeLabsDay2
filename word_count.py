8624f5317a1a6a16ff60321a0e5d166921505fb9
breday <brndnyokabi@gmail.com>
2017-02-08 16:24:27 +0300

    update on word_count.py

--- word_count.py
+++ word_count.py
@@ -1,17 +1,19 @@
 #using dict to determine word count 
 def word_frequency(string):
-	
+	#create dictonary tstore results
     word_dict = {}
 
+#code split the string 
     string = string.split(" ")
-
-    for x in string:
+ 
+    for x in string:#iterate through each value in list
         if x in word_dict:
-            word_dict[x] += 1
+            word_dict[x] += 1 #if value more than one, add one
         else:
             word_dict[x] = 1
 
     return word_dict
 
 
-print(word_frequency("// hello this is a multiline "))
+print(word_frequency("  "))
+
